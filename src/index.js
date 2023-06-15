1e6e507c5924ce008a3e2948b8f8820d5a14fb5b
Sandra G <sandyjtech@gmail.com>
2023-06-15 15:40:18 -0700

    created task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e) => {      
+   e.preventDefault();
+   createTask(e.target.newTaskDescription.value);
+   console.log(e.target.newTaskDescription.value);
+  form.reset();
+    });     
+
+  });
+
+function createTask(newTask) {
+  let p = document.createElement("p");
+  let button = document.createElement("button");
+  button.addEventListener("click", removeTask);
+
+  button.textContent = "Delete";
+  p.textContent = `${newTask}`;
+  p.appendChild(button);
+  document.querySelector("#list").appendChild(p);
+ }
+
+function removeTask(e){
+  e.target.parentNode.remove();
+}
